f1208cf8d343eb2ee8aae676105ea57e2c61f273
t-ho <t.ho26010@gmail.com>
2015-04-25 03:10:26 +1000

    Add CrinkleViewer.java, LaunchMode.java, PlaybackMode.java

---
+++ src/crinkle/CrinkleViewer.java
@@ -0,0 +1,29 @@
+
+package crinkle;
+
+/**
+ *
+ * @author ToanHo
+ */
+public class CrinkleViewer {
+
+    private LaunchMode launchMode;
+    
+    public CrinkleViewer() {
+        launchMode = new LaunchMode();
+        launchMode.setVisible(true);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new CrinkleViewer();
+            }
+        });
+    }
+    
+}
