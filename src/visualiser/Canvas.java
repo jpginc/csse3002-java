52ef4c2718215bc28f580f2f90715198dd985ca6
Joshua Graham <joshua.graham@jpginc.com.au>
2015-04-26 19:02:30 +1000

    playing around with coloring in things

--- src/visualiser/Canvas.java
+++ src/visualiser/Canvas.java
@@ -1,32 +1,36 @@
 package visualiser;
 
+import java.awt.Color;
 import java.awt.Component;
 
 import crinkle.PlaybackMode;
 import data.AccelReading;
 import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
+import de.jreality.scene.Appearance;
 import de.jreality.scene.IndexedFaceSet;
 import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.Viewer;
 import de.jreality.scene.data.Attribute;
 import de.jreality.scene.data.StorageModel;
+import de.jreality.shader.ShaderUtility;
+import static de.jreality.shader.CommonAttributes.*;
+import de.jreality.util.SceneGraphUtility;
+
 
 public class Canvas {
 	
-	IndexedFaceSet test = Primitives.sphere(10);
-	Integer pointsIndex = 0;
+	private IndexedFaceSet test = Primitives.sphere(10);
+	private Integer pointsIndex = 0;
 	private Viewer viewer;
+	private SceneGraphComponent world = SceneGraphUtility.createFullSceneGraphComponent("test");
 	
 	public Canvas() {
-		
-		SceneGraphComponent world = new SceneGraphComponent();
-		
 		world.setGeometry(test);
-
 		JRViewer jrViewer = JRViewer.createJRViewer(world);
 		jrViewer.startupLocal();
 		viewer = jrViewer.getViewer();
+		this.changeColor(null);
 	}
 		
 	public void mutate(int value) {
@@ -41,7 +45,16 @@ public class Canvas {
 	}
 	
 	public void changeColor(AccelReading accelReading) {
-		
+		Appearance ap = world.getAppearance();
+		ap.setAttribute(LINE_SHADER+"."+DIFFUSE_COLOR, Color.yellow);
+		ap.setAttribute(LINE_SHADER+"."+TUBE_RADIUS, .05);
+		ap.setAttribute(POINT_SHADER+"."+DIFFUSE_COLOR, Color.red);
+		ap.setAttribute(POINT_SHADER+"."+POINT_RADIUS, .1);
+		ap.setAttribute(POLYGON_SHADER+"."+SMOOTH_SHADING, false);
+		// turn on transparency for faces but keep tubes and spheres opaque
+		ap.setAttribute(TRANSPARENCY_ENABLED, true);
+		ap.setAttribute(OPAQUE_TUBES_AND_SPHERES, true);
+		ap.setAttribute(POLYGON_SHADER+"."+TRANSPARENCY, .4);
 	}
 
 	public Viewer getViewer() {
