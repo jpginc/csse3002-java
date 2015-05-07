e432b58f76fbd55ab2c03a5bf71258998e381194
Joshua Graham <joshua.graham@jpgautomation.com>
2015-05-07 15:23:44 +1000

    Added functions that mutate on sensor readings instead of random movement

--- src/visualiser/Canvas.java
+++ src/visualiser/Canvas.java
@@ -1,10 +1,8 @@
 package visualiser;
 
 import java.awt.Color;
-import java.awt.Component;
-
-import crinkle.PlaybackMode;
 import data.AccelReading;
+import data.SensorReading;
 import de.jreality.geometry.Primitives;
 import de.jreality.plugin.JRViewer;
 import de.jreality.scene.Appearance;
@@ -13,7 +11,6 @@ import de.jreality.scene.SceneGraphComponent;
 import de.jreality.scene.Viewer;
 import de.jreality.scene.data.Attribute;
 import de.jreality.scene.data.StorageModel;
-import de.jreality.shader.ShaderUtility;
 import static de.jreality.shader.CommonAttributes.*;
 import de.jreality.util.SceneGraphUtility;
 
@@ -23,6 +20,17 @@ public class Canvas {
 	Integer pointsIndex = 0;
 	Integer pointsYIndex = 0;
 	Integer timer = 0;
+	
+	//@ Josh G
+	//when moving a point this value indicates which axis to move along
+	private int axis = 0;
+	//there are 3 axis a point can move along
+	private int maxAxis = 3;
+	//when moving a point this value indicates which point will be moved
+	private int point = 0;
+	//this is the number of points in the sphere
+	private int pointMax = test.getNumPoints();
+
 	double random;
 	double diff;
 	private Viewer viewer;
@@ -41,7 +49,92 @@ public class Canvas {
 		//this.changeColor(null);
 		
 	}
+	
+	/*
+	 * This function is called by the visualizer to mutate the image one step
+	 * 
+	 * Whenever you want to use the next sensor reading call next()
+	 * 
+	 * @param reading
+	 *  The sensor reading to mutate with
+	 * 
+	 * @param stepCount
+	 *  The number of steps to break the sensor readings into
+	 *  
+	 */
+	public void mutate(SensorReading reading, int stepCount) {
+		movePoint(((double) reading.getFlex1()) / stepCount);
+		movePoint(((double) reading.getFlex2()) / stepCount);
+		changeColor(reading.getAccel(), stepCount);
+	}
+	
+	/*
+	 * This function changes the point that the mutate function works on
+	 * 
+	 * Call this whenever you change sensor reading
+	 * 
+	 * @param reverse
+	 * if set to true it changes to the previous point instead of the next one (for rewind)
+	 */
+	public void next(boolean reverse) {
+		point += reverse ? -1 : 1;
+		if(point == -1){
+			point = pointMax -1 ;
+		} else if(point == pointMax) {
+			point = 0;
+		}
 		
+		axis += reverse ? -1 : 1;
+		if(axis == -1) {
+			axis = maxAxis - 1;
+		} else if(axis == maxAxis) {
+			axis = 0;
+		}
+	}
+	
+	/*
+	 * Moves the point value distance on the current axis
+	 * 
+	 * @param value
+	 *  The distance to move the point
+	 */
+	private void movePoint(double value) {
+        double[][] points=new double[test.getNumPoints()][];
+		test.getVertexAttributes(Attribute.COORDINATES).toDoubleArrayArray(points);
+		points[point][axis] += value;
+		test.setVertexAttributes(Attribute.COORDINATES,StorageModel.DOUBLE_ARRAY.array(3).createReadOnly(points));
+
+	}
+
+	/*
+	 * changes the color of the sphere easing the color change according to the number of steps
+	 * 
+	 * todo this functions doesn't scale as steps gets larger!
+	 */
+	public void changeColor(AccelReading accelReading, int steps) {
+		Appearance ap = world.getAppearance();
+		
+		int stepZ = accelReading.getZ() / steps;
+		int stepX = accelReading.getX() / steps;
+		int stepY = accelReading.getY() / steps;
+
+        int oldBlue = base.getBlue();
+		int oldRed = base.getRed();
+		int oldGreen = base.getGreen();	
+		int newBlue, newRed, newGreen;
+
+		newBlue = (oldBlue + stepZ) % 255;
+        newRed = (oldRed + stepX) % 255;
+        newGreen = (oldGreen + stepY) % 255;					
+		
+		base = new Color(newRed, newGreen, newBlue);
+		 
+		ap.setAttribute(POLYGON_SHADER+"."+DIFFUSE_COLOR, base);
+	}
+
+	/*
+	 * moves a point and changes the color according to a random number
+	 */
 	public void mutate(int value) {
         double[][] points=new double[test.getNumPoints()][];
 		test.getVertexAttributes(Attribute.COORDINATES).toDoubleArrayArray(points);
@@ -129,9 +222,6 @@ public class Canvas {
 		ap.setAttribute(POLYGON_SHADER+"."+DIFFUSE_COLOR, base);
 		//ap.setAttribute(POLYGON_SHADER+"."+DIFFUSE_COLOR, newColor);
 	}
-
-
-
 	
 	public Viewer getViewer() {
 		return viewer;
