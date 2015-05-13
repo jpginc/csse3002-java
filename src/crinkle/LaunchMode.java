ec9692df2085024cf560aa24ef6c55ef0f5a7ed8
Josh <j.wise1995@me.com>
2015-05-13 12:02:09 +1000

    Added serial communication functionaloty between crinkle and viewer

--- src/crinkle/LaunchMode.java
+++ src/crinkle/LaunchMode.java
@@ -4,15 +4,23 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Toolkit;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileView;
+
+import org.zu.ardulink.Link;
+import org.zu.ardulink.RawDataListener;
 
 /**
  *
@@ -28,11 +36,42 @@ public class LaunchMode extends javax.swing.JFrame {
 	private javax.swing.JTextField txfFileName;
 	private javax.swing.JLabel pnlTop; // contain btnConnect
 	private javax.swing.JLabel pnlBottom; // contain lblFileName, txfFileName, btnOpen
+	private javax.swing.JButton btnReceive; //button that starts receiving live data from the crinkle
+    private javax.swing.JButton btnStop; 
+    private javax.swing.JButton btnConnect;
+    private Link link;
+    private List<String> portList = new ArrayList<String>();
+    private JComboBox comboPorts;
+    private List<String> dataArray = new ArrayList<String>();
+    private JFileChooser sfc = new JFileChooser();
+	
 
 	/**
 	 * Creates new form LaunchMode
 	 */
 	public LaunchMode() {
+		
+		link = Link.getDefaultInstance();
+		portList = link.getPortList();
+		
+		link.addRawDataListener(new RawDataListener() {
+    		@Override
+    		public void parseInput(String id, int numBytes, int[] message) {
+    			String received = "";
+    			for (int i = 0; i < numBytes; i++) {
+    				received += (char) message[i];
+    			}
+    			System.out.println(received.trim());
+    			if ("$_STOP_$".equals(received.trim())) {
+    				System.out.println("Saving data");
+    				saveData();
+    				dataArray.clear();
+    			} else if (!("$_START_$".equals(received.trim()))) {
+    				dataArray.add(received.trim());
+    			}
+    		}
+    	});
+		
 		initComponents();
 	}
 
@@ -60,6 +99,10 @@ public class LaunchMode extends javax.swing.JFrame {
 		lblStatus = new javax.swing.JLabel();
 		pnlTop = new JLabel();
 		pnlBottom = new JLabel();
+		btnReceive = new javax.swing.JButton();
+        btnStop = new javax.swing.JButton();
+        btnConnect = new javax.swing.JButton();
+        comboPorts = new javax.swing.JComboBox(portList.toArray());
 
 
 		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -74,6 +117,34 @@ public class LaunchMode extends javax.swing.JFrame {
 				btnSyncActionPerformed(evt);
 			}
 		});
+		
+		btnReceive.setFont(new java.awt.Font("Tahoma", 1, 14));
+        btnReceive.setText("Receive");
+        btnReceive.setPreferredSize(new java.awt.Dimension(140, 40));
+        btnReceive.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnReceiveActionPerformed(evt);
+            }
+        });
+        
+        btnStop.setFont(new java.awt.Font("Tahoma", 1, 14));
+        btnStop.setText("Stop");
+        btnStop.setEnabled(false);
+        btnStop.setPreferredSize(new java.awt.Dimension(140, 40));
+        btnStop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnStopActionPerformed(evt);
+            }
+        });
+        
+        btnConnect.setFont(new java.awt.Font("Tahoma", 1, 14));
+        btnConnect.setText("Connect");
+        btnConnect.setPreferredSize(new java.awt.Dimension(140, 40));
+        btnConnect.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConnectActionPerformed(evt);
+            }
+        });
 
 		txfFileName.setEnabled(false);
 		txfFileName.setPreferredSize(new java.awt.Dimension(300, 20));
@@ -99,7 +170,8 @@ public class LaunchMode extends javax.swing.JFrame {
 		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 0, 5));
 		getContentPane().add(pnlTop);
 		pnlTop.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 15));
-		pnlTop.add(btnSync);
+		pnlTop.add(btnConnect);
+        pnlTop.add(comboPorts);
 		pnlTop.add(lblStatus);
 		getContentPane().add(jSeparator1);
 		getContentPane().add(pnlBottom);
@@ -124,9 +196,44 @@ public class LaunchMode extends javax.swing.JFrame {
 	}
 
 	private void btnSyncActionPerformed(java.awt.event.ActionEvent evt) {
-		// TODO add your handling code here:
-		lblStatus.setText("Not implement yet");
-	}
+        // TODO add your handling code here:
+    	lblStatus.setText("Synchronising...");
+    	link.writeSerial("$_SYNC_$");
+    	lblStatus.setText("Finished Sync");
+    }
+	
+	private void btnReceiveActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+    	link.writeSerial("$_TRANSMIT_$");
+    	lblStatus.setText("Receiving Data");
+    	btnReceive.setEnabled(false);
+    	btnStop.setEnabled(true);
+    }
+    
+    private void btnStopActionPerformed(java.awt.event.ActionEvent evt) {
+        // TODO add your handling code here:
+    	link.writeSerial("$_STOP_$");
+    	btnStop.setEnabled(false);
+    	btnReceive.setEnabled(true);
+    	lblStatus.setText("Receive Completed");
+        //save the data set received
+    }
+    
+    private void btnConnectActionPerformed(java.awt.event.ActionEvent evt) {
+    	boolean status = connect();
+    	if (status) {
+    		pnlTop.remove(lblStatus);
+    		pnlTop.remove(btnConnect);
+    		pnlTop.remove(comboPorts);
+        	pnlTop.add(btnSync);
+            pnlTop.add(btnReceive);
+            pnlTop.add(btnStop);
+            pnlTop.add(lblStatus);
+            validate();
+    	} else {
+    		lblStatus.setText("Crinkle not detected on " + comboPorts.getSelectedItem().toString());
+    	}
+    }
 
 	private void btnOpenActionPerformed(java.awt.event.ActionEvent evt) {
 		// TODO add your handling code here:
@@ -222,4 +329,49 @@ public class LaunchMode extends javax.swing.JFrame {
 		}
 		return extension;
 	}
+	
+	
+	private void saveData() {
+    	String filePath = "";
+    	PrintWriter writer = null;
+    	System.out.println("Size of stored array = " + dataArray.size());
+    	sfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        int status = sfc.showSaveDialog(LaunchMode.this);
+        System.out.println("status =" + status);
+        if (status == JFileChooser.APPROVE_OPTION) {
+        	File fileToSave = sfc.getSelectedFile();
+        	filePath = fileToSave.getAbsolutePath();
+        	System.out.println("Saving file as: " + filePath);
+        	try {
+				writer = new PrintWriter(fileToSave);
+			} catch (FileNotFoundException e) {
+				System.out.println("file not found");
+				e.printStackTrace();
+			}
+        	for (int i = 0; i < dataArray.size(); i++) {
+        		//save each line
+        		writer.println(dataArray.get(i));
+        		writer.flush();
+        	}
+        	writer.close();
+        }
+    }
+    
+    private boolean connect() {
+    	boolean connected = false;
+    	
+    	try {
+    		if (portList != null && portList.size() > 0) {
+    			String port = comboPorts.getSelectedItem().toString();
+    			System.out.println("Connecting to port " + port);
+    			connected = link.connect(port);
+    		} else {
+    			System.out.println("No port found!");
+    		}
+    	} catch (Exception e) {
+    		 e.printStackTrace();
+    	}
+    	return connected;
+    }
+	
 }
