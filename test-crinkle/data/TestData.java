3be3f70109b9b4457c277fc45b2564eaf8698ce0
Miya Lu <m1ya.lomiu@gmail.com>
2015-05-17 19:20:17 +1000

    fix classpath

--- test-crinkle/data/TestData.java
+++ test-crinkle/data/TestData.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*; 
+//import static org.mockito.Mockito.*; 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
